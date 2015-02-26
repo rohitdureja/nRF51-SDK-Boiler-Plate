08e2919c81ca68116d03cdd7ad41fcbcc8f4cd67
rohitdureja <rohit@seas.upenn.edu>
2015-02-26 17:33:04 -0500

    added ADC example

---
+++ adc_example/main.c
@@ -0,0 +1,109 @@
+/* 
+* ADC: Sample voltage on GPIO P0.05 at 10 Hz and send to UART
+ */
+
+#include <stdbool.h>
+#include "nrf.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+#include "simple_uart.h"
+
+// Defines for LEDs
+#define LED_0 18
+#define LED_1 19
+
+// Defines for UART pins
+#define RX_PIN_NUMBER	11
+#define TX_PIN_NUMBER	9
+
+// Defines for counter
+#define LFCLK_FREQUENCY           (32768UL)                 /*!< LFCLK frequency in Hertz, constant */
+#define RTC_FREQUENCY             (10UL)                     /*!< required RTC working clock RTC_FREQUENCY Hertz. Changable */
+#define COUNTER_PRESCALER         ((LFCLK_FREQUENCY/RTC_FREQUENCY) - 1)  /* f = LFCLK/(prescaler + 1) */
+
+// Enable external 16MHz crystal oscillator
+static void hfclk_config(void)
+{
+	/* Start 16 MHz crystal oscillator */
+  NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_HFCLKSTART = 1;
+
+  /* Wait for the external oscillator to start up */
+  while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) {
+	}
+}
+// Enable low frequency oscillator
+static void lfclk_config(void)
+{
+  NRF_CLOCK->LFCLKSRC = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+  NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_LFCLKSTART = 1;
+	
+	// Wait for the low frequency clock to start
+  while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) {}
+  NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+}
+
+// Configure the RTC
+static void rtc_config(void)
+{
+  NRF_RTC0->PRESCALER = COUNTER_PRESCALER;                   // Set prescaler to a TICK of RTC_FREQUENCY
+  NRF_RTC0->EVTENSET = RTC_EVTENSET_TICK_Msk;                // Enable TICK event	
+	NRF_RTC0->TASKS_START = 1;                                 // Start RTC0
+}
+
+// Cofigure the PPI Channel
+static void ppi_init(void)
+{
+  // Configure PPI channel 3 to start ADC task
+  NRF_PPI->CH[3].EEP = (uint32_t)&NRF_RTC0->EVENTS_TICK;
+  NRF_PPI->CH[3].TEP = (uint32_t)&NRF_ADC->TASKS_START;
+
+  // Enable PPI channel 3
+  NRF_PPI->CHEN = (PPI_CHEN_CH3_Enabled << PPI_CHEN_CH3_Pos);
+}
+
+// Configures and enables the ADC
+void ADC_init(void)
+{	
+	/* Enable interrupt on ADC sample ready event*/		
+	NRF_ADC->INTENSET = ADC_INTENSET_END_Msk;
+	NVIC_EnableIRQ(ADC_IRQn);	
+	
+	// config ADC
+	NRF_ADC->CONFIG	= (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos) 								/* Bits 17..16 : ADC external reference pin selection. */
+										| (ADC_CONFIG_PSEL_AnalogInput6 << ADC_CONFIG_PSEL_Pos)									/*!< Use analog input 6 as analog input (P0.05). */
+										| (ADC_CONFIG_REFSEL_VBG << ADC_CONFIG_REFSEL_Pos)											/*!< Use internal 1.2V bandgap voltage as reference for conversion. */
+										| (ADC_CONFIG_INPSEL_AnalogInputNoPrescaling << ADC_CONFIG_INPSEL_Pos) 	/*!< Analog input specified by PSEL with no prescaling used as input for the conversion. */
+										| (ADC_CONFIG_RES_8bit << ADC_CONFIG_RES_Pos);													/*!< 8bit ADC resolution. */ 	
+	
+	
+	/* Enable ADC*/
+	NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Enabled;	
+}
+
+/* Interrupt handler for ADC data ready event. It will be executed when ADC sampling is complete */
+void ADC_IRQHandler(void)
+{
+	/* Clear dataready event */
+  NRF_ADC->EVENTS_END = 0;	
+
+  /* Write ADC result to UART */
+ 	simple_uart_put(NRF_ADC->RESULT); 
+}
+
+int main(void)
+{
+	hfclk_config();     //Enable 16MHz crystal for maximum ADC accuracy. Comment out this line to use internal 16MHz RC instead, which saves power.
+	lfclk_config();			//Configure 32kHz clock, required by the RTC timer
+  rtc_config();				//Configure RTC timer
+	ppi_init();					//Configure PPI channel, which connects RTC TICK event and ADC START task
+	ADC_init();					//Configure the ADC
+	simple_uart_config(0, TX_PIN_NUMBER, 0, RX_PIN_NUMBER, false);	//Configure UART
+	
+  while (true)
+  {	
+		//Put CPU to sleep while waiting for interrupt
+		__WFI();
+  }
+}
